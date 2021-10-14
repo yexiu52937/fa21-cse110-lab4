12712ee26f996f66f90f7af0950e078b0925de4a
Yifan Hou <y7hou@ucsd.edu>
2021-10-14 16:42:45 -0700

    changed sth

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -7,11 +7,11 @@ let statistics = {
     rareCars: 2
 }
 
-for (const property in statistics) {
-    if (property[0]=='r') {
-        console.log(statistics[property]);
+for (const car in statistics) {
+    if (car[0]=='r') {
+        console.log(statistics[car]);
     }
-    else if (statistics[property]%2==1) {
-        console.log(statistics[property]);
+    else if (statistics[car]%2==1) {
+        console.log(statistics[car]);
     }
 }
